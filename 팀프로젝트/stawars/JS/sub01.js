6d9695bcfa3b4349b9471c2209debadf716769ae
dong13475 <travel1347@gmail.com>
2023-02-27 11:56:44 +0900

    학습

--- 팀프로젝트/stawars/JS/sub01.js
+++ 팀프로젝트/stawars/JS/sub01.js
@@ -5,9 +5,16 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const tsecI = document.querySelector(".tsecI");
   const tsecT = document.querySelector(".tsecT");
-  const mytxt = `
-          안녕하세요
+  const mytxt = 
+  `
+  Star Wars: The Phantom Menace (Episode I) \n
+  Rating: PG \n
+  Runtime: 2 hr 16min \n
+  Release Date: May 19, 1999 \n
+  Genre: \n
+  Action, Adventure, Fantasy, Live Action, Science Fiction
   `;
+
   autoI = setTimeout(() => {
     tsecI.style.opacity = "1";
     tsecI.style.transition = ".4s ease-in-out";
@@ -17,10 +24,13 @@ window.addEventListener("DOMContentLoaded", () => {
   let idx = 0;
   for (let x of mytxt) {
     if (x === " ") x = "&nbsp;&nbsp;&nbsp";
-    hcode += 
-          `<span style="transition-delay:${idx*0.1}s">${x}</span>`;
-
+    if (x === "\n") x = "<br>";
+    // else if (x === "br") x = "<br>";
+    hcode += `<span style="transition-delay:${idx * 0.05}s">${x}</span>`;
     idx++;
   } ////// for of //////
   tsecT.innerHTML = hcode;
+  setTimeout(() => {
+    tsecT.classList.add("on");
+  },500);
 }); ///////// 로드구역 /////////////////////////////
