ab0b5229ba7587a64473bcbb27991c4fefb9a7df
dong13475 <travel1347@gmail.com>
2023-02-27 16:21:02 +0900

    학습

--- 팀프로젝트/stawars/JS/sub01.js
+++ 팀프로젝트/stawars/JS/sub01.js
@@ -6,31 +6,32 @@ window.addEventListener("DOMContentLoaded", () => {
   const tsecI = document.querySelector(".tsecI");
   const tsecT = document.querySelector(".tsecT");
   const mytxt = 
-  `
-  Star Wars: The Phantom Menace (Episode I) \n
-  Rating: PG \n
-  Runtime: 2 hr 16min \n
-  Release Date: May 19, 1999 \n
-  Genre: \n
-  Action, Adventure, Fantasy, Live Action, Science Fiction
+  `Star Wars: The Phantom Menace (Episode I) ㅁ
+  Rating: PG ㅁ
+  Runtime: 2 hr 16min ㅁ
+  Release Date: May 19, 1999 ㅁ
+  Genre: ㅁ
+  Action, Adventure, Fantasy, Science Fiction
   `;
 
-  autoI = setTimeout(() => {
-    tsecI.style.opacity = "1";
-    tsecI.style.transition = ".4s ease-in-out";
-  }, 2000);
-
-  let hcode = "";
-  let idx = 0;
-  for (let x of mytxt) {
-    if (x === " ") x = "&nbsp;&nbsp;&nbsp";
-    if (x === "\n") x = "<br>";
-    // else if (x === "br") x = "<br>";
-    hcode += `<span style="transition-delay:${idx * 0.05}s">${x}</span>`;
-    idx++;
-  } ////// for of //////
-  tsecT.innerHTML = hcode;
-  setTimeout(() => {
-    tsecT.classList.add("on");
-  },500);
+  const showIt = ()=>{
+    autoI = setTimeout(() => {
+      tsecI.style.opacity = "1";
+      tsecI.style.transition = ".4s ease-in-out";
+    }, 2000);
+  
+    let hcode = "";
+    let idx = 0;
+    for (let x of mytxt) {
+      if (x === " ") x = "&nbsp;&nbsp";
+      if (x === "ㅁ") x = "\n";
+      hcode += `<span style="transition-delay:${idx * 0.03}s">${x}</span>`;
+      idx++;
+    } ////// for of //////
+    tsecT.innerHTML = hcode;
+    setTimeout(() => {
+      tsecT.classList.add("on");
+    },500);
+  };
+  showIt();
 }); ///////// 로드구역 /////////////////////////////
