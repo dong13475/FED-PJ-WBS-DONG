13210246373ce501603ca5d7b3b0d25705895c58
dong13475 <travel1347@gmail.com>
2023-03-15 16:30:13 +0900

    학습

--- 01.웹프로젝트/03.구현소스/JS/main.js
+++ 01.웹프로젝트/03.구현소스/JS/main.js
@@ -4,20 +4,27 @@
 window.addEventListener("DOMContentLoaded",()=>{
   console.log("로딩완료!");
   
-  const gnb = document.querySelectorAll(".gnb>ul>li");
-  // console.log(gnb);
-  
-  // GNB 두번째 li에 마우스엔터시 smenu등장
-  let smenu = gnb[1];
-  let qq = 
-  console.log(smenu);
-  smenu.onmouseenter = () => {
 
-    let hv = smenu.clientheight + "px";
-    console.log(hv);
+  const gnb_li = document.querySelectorAll(".gnb>ul>li");
+  // console.log(gnb_li);
+  let gg = gnb_li[1];
+  // console.log(gg);
+  const smenu = document.querySelector(".smenu");
+  // console.log(smenu);
+  let hv = smenu.querySelector(".smenu ol").clientHeight;
+  console.log(hv);
+  // GNB 두번째 li에 마우스엔터시 smenu등장
+  gg.onmouseenter = () => {
+    smenu.style.opacity = 1;
+    smenu.style.height = hv + "px";
+  }
+  gg.onmouseleave = () => {
+    smenu.style.opacity = 0;
+    smenu.style.height = 0;
   }
 
-    
+
+  
 
 }); ////////////////// 로드구역 ///////////////////
 //////////////////////////////////////////////////
