e8f96d7545214bac449b50263733838475228328
dong13475 <travel1347@gmail.com>
2023-03-20 10:53:32 +0900

    학습

--- 01.웹프로젝트/03.구현소스/JS/main.js
+++ 01.웹프로젝트/03.구현소스/JS/main.js
@@ -219,9 +219,11 @@ window.addEventListener("DOMContentLoaded",()=>{
       hcode += `
       <div class="item itbx1">
         <a href="#">
-          <div class="iimg">
-            <img src="./images/item_imgs/ALL/${gdsObj[x]["이미지1"]}.jpg">
-            <img src="./images/item_imgs/ALL/${gdsObj[x]["이미지2"]}.jpg">
+          <div class="iimg_bx">
+            <div class="iimg">
+              <img src="./images/item_imgs/ALL/${gdsObj[x]["이미지1"]}.jpg">
+              <img src="./images/item_imgs/ALL/${gdsObj[x]["이미지2"]}.jpg">
+            </div>
           </div>
         </a>
         <div class="itxt">
