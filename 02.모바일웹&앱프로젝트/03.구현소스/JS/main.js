367058cb3f8f8de4d21773c7bf5a19c2ca6e5c13
dong13475 <travel1347@gmail.com>
2023-04-26 14:37:55 +0900

    학습

--- 02.모바일웹&앱프로젝트/03.구현소스/JS/main.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/JS/main.js
@@ -4,15 +4,17 @@ $(()=>{ /////////// JQB /////////////
 
   // 햄버거버튼 클릭시 메뉴 활성화
   $(".ham").on("click",function(){
-    // $(this).addClass("on");
     $(this).toggleClass("on");
     $(".gnbbx").toggleClass("on");
-
-    // if($(".gnbbx").index("on")){
-    //   $(".gnbbx").show();
-    // }
-    // else{
-    //   $(".gnbbx").hide();
-    // }
   }); ///////// click //////////
+
+  // 서브메뉴 숨기기 -> display:none 처리
+  $(".smenu").hide();
+  
+  // GNB li 클릭시 서브메뉴 등장
+  $(".gnb>ul>li").click(function(){
+    $(this).find(".smenu").stop().slideToggle(400);
+  }); ////////// click /////////
+
+  
 }); //////////// JQB ///////////////
