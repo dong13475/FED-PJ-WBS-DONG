104b8c0f9349dba0354d9493cb10654f0a23759d
dong13475 <travel1347@gmail.com>
2023-05-12 16:45:09 +0900

    학습

--- 02.모바일웹&앱프로젝트/03.구현소스/JS/item.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/JS/item.js
@@ -1,73 +1,9 @@
 // 프라이탁 elevator 서브JS - ele.js
 
+import topNav from "./topNav.js";
+
 $(() => {
-  /******************** 상단 네비영역 ********************/
-  // 햄버거버튼 클릭시 메뉴 활성화
-  $(".ham").on("click", function () {
-    // $(this).toggleClass("on").css({left:"80px"}).parents(".btbt").css({width:"130px"});
-    $(this).toggleClass("on");
-    $(".gnbbx").toggleClass("on");
-    // .gnbbx에 클래스 on일때 css 효과
-    if ($(".gnbbx").hasClass("on")) {
-      $(".gnb>ul>li, .sns, .sName").css({
-        opacity: "1",
-        transition: ".4s",
-      }); ///// css /////
-    } else {
-      $(".gnb>ul>li, .sns, .sName").css({
-        opacity: "0",
-        transition: ".4s",
-      }); ///// css /////
-      // $(".smenu").hide();
-    }
-  }); ///////// click //////////
-
-  // 검색버튼 클릭시 input 보이기
-  $(".sech>div").on("click", function () {
-    $(".sech").toggleClass("on");
-  });
-
-  // 서브메뉴 숨기기 -> display:none 처리
-  $(".smenu").hide();
-
-  // GNB li 클릭시 서브메뉴 등장
-  $(".gnb>ul>li").click(function () {
-    // 서브메뉴 등장
-    $(this).find(".smenu").stop().slideToggle(400);
-    // 화살표 방향
-    $(this).find(".arrow").stop().toggleClass("on");
-  }); ////////// click /////////
-
-  // GNB li 오버시 밑줄 애니 클래스 "on"추가
-  $(".gnb>ul>li").each((idx, ele) => {
-    $(ele).hover(
-      function () {
-        $(ele).find("a").removeClass("on2").addClass("on");
-      },
-      function () {
-        $(ele).find("a").removeClass("on").addClass("on2");
-      }
-    );
-  }); ////////// each /////////////
-
-  // 서브메뉴 마우스 오버시 애니
-  $(".smenu>ol>li>a").each((idx, ele) => {
-    $(ele).hover(
-      function () {
-        // 오버시
-        $(this).css({
-          transform: "translateX(3%) scale(1.03)",
-        });
-      },
-      function () {
-        // 아웃시
-        $(this).css({
-          transform: "scale(1)",
-        });
-      }
-    );
-  }); /////// each //////////
-  /******************** 상단 네비영역 끝 ********************/
+  
 
   
 }); ///////////// JQB /////////////////
