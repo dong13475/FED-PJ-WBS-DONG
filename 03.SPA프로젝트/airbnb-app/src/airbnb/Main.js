dc2631096db99ee06b91c200afe39acf8878eae3
Tom B. Parker <tombap8@hotmail.com>
2023-06-27 17:32:58 +0900

    ss

--- 03.SPA프로젝트/airbnb-app/src/airbnb/Main.js
+++ 03.SPA프로젝트/airbnb-app/src/airbnb/Main.js
@@ -2,34 +2,75 @@
 import Items from './modules/Items';
 import item_data from './data/items';
 import Airplane from './modules/Airplane';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import './css/main.css';
+import { useEffect, useState } from 'react';
 
 const Main = (props) => {
+
+  // 라우터 이동
+  const goPage = useNavigate();
+
+  const [mainSts,setMainSts] = useState(0);
+
+  const chgIntro = (e) => {
+    let txt = e.target.innerText.trim();
+    console.log(txt);
+    setMainSts(2);
+    setTimeout(()=>{
+      setMainSts(1);
+      goPage('/'+txt,{});
+    },7000);
+
+  }; ////////////// chgIntro ///////////
+
+  const chkFn = () => {
+    console.log("여기링크");
+    let tg = 
+    document.querySelectorAll(".slider a");
+    console.log(tg);
+    tg.forEach(ele=>ele.onclick=()=>{
+      setMainSts(1);
+      console.log("링크!!!");
+    });
+  }; ////////// chkFn /////////////////
+
+  useEffect(chkFn,[]);
+
   return (
     <>
-      <Items cat={props.sub} />
-      <Airplane />
-      <section className='mainbx'>
-        {/* 메인박스영역 */}
-        <div className='mbx'>
-          {/* 자연 */}
-          <div className='nature'>
-            <Link to='/자연'>
-              {/* <Items cat={props.sub} /> */}
-              자연
-            </Link>
+      {
+        mainSts==1 && 
+        <Items cat={props.sub} />
+      }
+      {
+        mainSts==2 &&
+        <Airplane />
+
+      }
+      {
+        mainSts==0 &&
+        <section className='mainbx'>
+          {/* 메인박스영역 */}
+          <div className='mbx'>
+            {/* 자연 */}
+            <div className='nature'>
+              <a href='#' onClick={chgIntro}>
+                {/* <Items cat={props.sub} /> */}
+                자연
+              </a>
+            </div>
+            {/* 건축 */}
+            <div className='build'>
+              <a href='#' onClick={chgIntro}>
+                {/* <Items cat={props.sub} /> */}
+                건축
+              </a>
+            </div>
           </div>
-          {/* 건축 */}
-          <div className='build'>
-            <Link to='/'>
-              {/* <Items cat={props.sub} /> */}
-              건축
-            </Link>
-          </div>
-        </div>
-      </section>
+        </section>
+      }
     </>
   );
 }; ////////////// Main 컴포넌트 //////////////
--- 03.SPA프로젝트/airbnb-app/src/airbnb/modules/Items.js
+++ 03.SPA프로젝트/airbnb-app/src/airbnb/modules/Items.js
@@ -21,6 +21,49 @@ function jqFn() {
 function Items(props) {
   const idata = item_data[props.cat];
   console.log("idata:",idata);
+  if(props.cat=="자연" || props.cat=="건축"){
+    let temp = Object.keys(item_data);
+    console.log("자연/건축 키배열:",temp);
+
+    return(
+        temp.map((tv,ti)=>
+        <section className="itemList" key={ti}> 
+        {/* 각각아이템 */}
+        {item_data[tv].map((v, i) => (
+          v.main == props.cat &&
+          <Link to="/여행은살아보는거야" state={
+            {
+              src:v.src,
+              iname:v.iname,
+              iloca:v.iloca,
+              ihost:v.ihost,
+              ietc:v.ietc,
+              inum:v.inum,
+              gmap:v.gmap,
+              main:v.main
+            }
+          } key={i}>
+          <div className="item">
+            <div className="itemImg">
+              <SwiperItem seq={v.inum} cat={props.cat} src={v.src} />
+            </div>
+            {/* 아이템 타이틀 */}
+            <div className="itemTit">
+              <ul>
+                {/* 아이템 이름 */}
+                <li className="iname">{v.iname}</li>
+                {/* 아이템 위치 */}
+                <li className="iloca">{v.iloca}</li>
+              </ul>
+            </div>
+          </div>
+          </Link>
+        ))}
+      </section>
+        
+        )
+    );
+  }
 
   let mdt = idata.main;
   console.log("mdt:",mdt);
